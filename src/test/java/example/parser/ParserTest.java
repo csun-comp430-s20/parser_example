659e011bbd8b5621c60a59bf492cd113234c9f7d
Kyle Dewey <kyle.dewey@csun.edu>
2020-03-04 17:55:07 -0800

    added remaining tests; now getting 100% line and branch coverage in the parser

--- src/test/java/example/parser/ParserTest.java
+++ src/test/java/example/parser/ParserTest.java
@@ -16,6 +16,65 @@ public class ParserTest {
     public void emptyDoesNotParse() throws ParseException {
         assertParses(null);
     }
+
+    @Test
+    public void integerParses() throws ParseException {
+        assertParses(new IntegerExp(123), new IntegerToken(123));
+    }
+
+    @Test
+    public void variableParses() throws ParseException {
+        assertParses(new VariableExp("foo"), new VariableToken("foo"));
+    }
+
+    @Test
+    public void parensParse() throws ParseException {
+        assertParses(new VariableExp("foo"),
+                     new LeftParenToken(),
+                     new VariableToken("foo"),
+                     new RightParenToken());
+    }
+
+    @Test
+    public void ifParses() throws ParseException {
+        assertParses(new IfExp(new IntegerExp(1),
+                               new IntegerExp(2),
+                               new IntegerExp(3)),
+                     new IfToken(),
+                     new LeftParenToken(),
+                     new IntegerToken(1),
+                     new RightParenToken(),
+                     new IntegerToken(2),
+                     new ElseToken(),
+                     new IntegerToken(3));
+    }
+
+    @Test
+    public void plusParses() throws ParseException {
+        assertParses(new PlusExp(new IntegerExp(1), new IntegerExp(2)),
+                     new IntegerToken(1),
+                     new PlusToken(),
+                     new IntegerToken(2));
+    }
+
+    @Test
+    public void plusIsLeftAssociative() throws ParseException {
+        assertParses(new PlusExp(new PlusExp(new IntegerExp(1),
+                                             new IntegerExp(2)),
+                                 new IntegerExp(3)),
+                     new IntegerToken(1),
+                     new PlusToken(),
+                     new IntegerToken(2),
+                     new PlusToken(),
+                     new IntegerToken(3));
+    }
+
+    @Test(expected = ParseException.class)
+    public void missingIntegerGivesParseError() throws ParseException {
+        assertParses(null,
+                     new IntegerToken(1),
+                     new PlusToken());
+    }
 } // ParserTest
     
         
